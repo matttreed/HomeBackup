23057c98dfec3a4169f3c3fe27408214eca9838f
Matt Reed <matthewreed02@gmail.com>
2020-12-28 15:12:44 -0800

    Initial Commit

---
+++ Home/Controller/TitleScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home
+//
+//  Created by Matthew Reed on 12/28/20.
+//
+
+import UIKit
+
+class TitleScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
