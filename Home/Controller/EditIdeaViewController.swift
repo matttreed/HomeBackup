b95fe2cd63edfe0cd732a566b081264322923a2f
Matt Reed <matthewreed02@gmail.com>
2020-12-28 18:48:22 -0800

    added explanation functionality

--- Home/Controller/EditIdeaViewController.swift
+++ Home/Controller/EditIdeaViewController.swift
@@ -12,6 +12,9 @@ class EditIdeaViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var groupTextView: UIView!
     @IBOutlet weak var ideaTextView: UITextView!
+    @IBOutlet weak var explanationTextView: UITextView!
+    @IBOutlet weak var addExplanationButton: UIButton!
+    @IBOutlet weak var explanationTextViewContainer: UIView!
     
     var parentVC: IdeasViewController? = nil
     var idea: Idea? = nil
@@ -20,7 +23,13 @@ class EditIdeaViewController: UIViewController {
         super.viewDidLoad()
         
         idea = parentVC?.ideasArray[parentVC!.selectedIndex]
-        ideaTextView.text = idea?.idea!
+        ideaTextView.text = idea!.idea
+        if idea!.explanation == nil {
+            explanationTextViewContainer.isHidden = true
+        } else {
+            addExplanationButton.isHidden = true
+            explanationTextView.text = idea?.explanation!
+        }
         
         groupTextView.layer.cornerRadius = 10
         groupTextView.layer.borderWidth = 1
@@ -29,6 +38,11 @@ class EditIdeaViewController: UIViewController {
     
     @IBAction func saveButtonPressed(_ sender: UIButton) {
         idea!.idea = ideaTextView.text
+        if explanationTextView.text == "" {
+            idea!.explanation = nil
+        } else {
+            idea!.explanation = explanationTextView.text
+        }
         parentVC?.ideasTable.reloadData()
         parentVC?.saveData()
         self.dismiss(animated: true, completion: nil)
@@ -46,6 +60,11 @@ class EditIdeaViewController: UIViewController {
     @IBAction func deleteButtonPressed(_ sender: UIButton) {
         deleteIdea()
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addExplanationButtonPressed(_ sender: UIButton) {
+        explanationTextViewContainer.isHidden = false
+        addExplanationButton.isHidden = true
     }
     
     func deleteIdea() {
